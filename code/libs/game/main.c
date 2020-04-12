6d4487ddb85f6b2c18e7aa47796c261b7b25ebff
Davi Guizani Carvalho <daviguizanicarvalho@gmail.com>
2020-04-12 20:54:10 -0300

    Adicionada a lib bool
    Implementada a biblioteca bool
    
    Importada a biblioteca bool na biblioteca game
    
    Criada a função fazerJogada

--- code/libs/game/main.c
+++ code/libs/game/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "../bool/main.c"
 
 typedef struct posicao
 {
@@ -21,4 +22,10 @@ void limparTabuleiro(Tab *tab)
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             tab -> tabuleiro[i][j] = ' ';
+}
+
+Bool fazerJogada(Tab *tab, Pos pos)
+{
+    if (!(pos.x >= 1 && pos.x <= 3))
+        return false;
 }

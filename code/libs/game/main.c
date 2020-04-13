66c56e7da79f1317d61419684d9567cbff1a1455
Davi Guizani Carvalho <daviguizanicarvalho@gmail.com>
2020-04-12 21:57:44 -0300

    lib game
    Implementada a função fazerJogada
    
    adicionado comentários acima das funções

--- code/libs/game/main.c
+++ code/libs/game/main.c
@@ -12,20 +12,62 @@ typedef struct tabuleiro
     char tabuleiro[3][3];
 } Tab;
 
+/*
+ * Função para criar um novo tabuleiro
+ * 
+ * return Tab *: se conseguiu criar o tabuleiro
+ * return NULL: se não conseguiu criar o tabuleiro
+*/
 Tab *criarTabuleiro()
 {
     return (Tab *) malloc(sizeof(Tab));
 }
 
-void limparTabuleiro(Tab *tab)
+/*
+ * Função para colocar todas as casas do tabuleiro como espaço vazio
+ * 
+ * param tab: ponteiro para Tab
+ * 
+ * return true: se conseguiu preencher
+ * return false: se não conseguiu preencher
+*/
+Bool limparTabuleiro(Tab *tab)
 {
+    if (tab == NULL)
+        return false;
+    
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             tab -> tabuleiro[i][j] = ' ';
+    
+    return true;
 }
 
-Bool fazerJogada(Tab *tab, Pos pos)
+/*
+ * Função para jogar X ou O no tabuleiro
+ * 
+ * param tab: ponteiro para Tab
+ * param pos: posicao a ser jogada
+ * param jogador: jogador da vez, X = 1, O = 2
+*/
+Bool fazerJogada(Tab *tab, Pos pos, char jogador)
 {
+    if (tab == NULL)
+        return false;
+
+    if (!(jogador == 'X' || jogador == 'x' || jogador == 'O' || jogador == 'o'))
+        return false;
+    
     if (!(pos.x >= 1 && pos.x <= 3))
         return false;
+    
+    if (!(pos.y >= 1 && pos.y <= 3))
+        return false;
+    
+    if (tab -> tabuleiro[pos.x][pos.y] == ' ')
+        return false;
+    
+    tab -> tabuleiro[pos.x][pos.y] = (jogador == 'X' || jogador == 'x') ? 'X' : 'O';
+    
+    return true;
 }
